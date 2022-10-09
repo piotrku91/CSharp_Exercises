aee66e334af1b739d9ddcf503b21dbf3b61a9b2a
piotrku <314159+Virtlink@@users.noreply.github.com>
2022-10-09 15:45:56 +0200

    tictac area

---
+++ ex11/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Size definition
+            const int width = 3;
+            const int height = 3;
+
+            string[,] cells = new string[width, height];
+
+            Console.WriteLine("Witaj w programie Kółko i Krzyżyk!");
+
+            // Getting data from user
+            for (int i=0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    bool valid_value = false;
+                    while (!valid_value)
+                    {
+                        Console.Write("Podaj stan " + ((i * height + j) + 1) + "-ego pola: ");
+                        string new_value = Console.ReadLine();
+
+                        if (new_value == "O" || new_value == "X" || new_value == " ")
+                        {
+                            cells[i, j] = new_value;
+                            valid_value = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid value. Try again.");
+                        }
+                    }
+                }
+            }
+
+            Console.Write("\n\n"); // Some dummy line
+
+            // Print matrix
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write(" ");
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(cells[i,j]);
+                    if (j != width - 1)
+                    {
+                        Console.Write(" | ");
+                    };
+                }
+                Console.Write("\n");
+                if (i != height - 1)
+                {
+                    Console.WriteLine("---+---+---");
+                };
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
