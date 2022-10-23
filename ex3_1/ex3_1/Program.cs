4a7e67a0814d16e8b7588312a5483cce6568727a
piotrku <314159+Virtlink@@users.noreply.github.com>
2022-10-23 14:12:06 +0200

    ex 3 begin

---
+++ ex3_1/ex3_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę n: ");
+            int n = 0;
+            bool validation = int.TryParse(Console.ReadLine(), out n);
+
+            int m = 0;
+            Console.WriteLine("Podaj liczbę m: ");
+            validation = validation && int.TryParse(Console.ReadLine(), out m);
+
+            if (validation)
+            {
+                Console.WriteLine("Wynik wyrażenia to " + ((input_a * input_b) / (input_a + input_b)));
+                return;
+            }
+            Console.WriteLine("Nieprawidłowa liczba lub znak");
+        }
+    }
+}
---
+++ ex3_2/ex3_2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3_2
+{
+    class Program
+    {
+
+        enum DisplayOrder { Normal, Reversed};
+        static void PrintInRow(int[] array, int count, DisplayOrder display_order)
+        {
+            if (display_order == DisplayOrder.Normal)
+            {
+                Console.WriteLine("Wiersz: ");
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wiersz (odwrócony): ");
+                for (int i = count - 1; i >= 0; i--)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+                Console.Write("\n");
+        }
+
+        static void PrintInColumn(int[] array, int count)
+        {
+            Console.WriteLine("Kolumna: ");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(array[i] + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int max_count = 0;
+            Console.WriteLine("Podaj ilość liczb: ");
+
+            if (!int.TryParse(Console.ReadLine(), out max_count))
+                {
+                Console.WriteLine("Nieprawidłowa ilość liczb");
+                return;
+            }
+
+            int[] input = new int[max_count];
+            bool validation = true;
+
+            for (int i = 0; i < max_count; i++)
+            {
+                Console.WriteLine("Podaj liczbę {0}: ", i);
+                validation = validation && int.TryParse(Console.ReadLine(), out input[i]);
+
+                if (!validation) { break; };
+            }
+
+            Console.Write("\n");
+
+            if (validation)
+            {
+                PrintInRow(input, max_count, DisplayOrder.Normal);
+                PrintInColumn(input, max_count);
+                PrintInRow(input, max_count, DisplayOrder.Reversed);
+            }
+
+            else
+            {
+                Console.WriteLine("Nieprawidłowa liczba lub znak");
+            }
+            Console.ReadKey();
+        }
+    }
+}
